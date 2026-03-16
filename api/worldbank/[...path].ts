13dcfac389f3eeab6386b372f8252fa0acf175b3
Kush <145403761+kush0o7@users.noreply.github.com>
2026-03-16 00:13:18 -0700

    changes

--- api/worldbank/[...path].ts
+++ api/worldbank/[...path].ts
@@ -1,6 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: any, res: any) {
   const path = (req.query.path as string[])?.join('/') ?? ''
   const target = `https://api.worldbank.org/${path}${req.url?.includes('?') ? req.url.slice(req.url.indexOf('?')) : ''}`
   const response = await fetch(target)
